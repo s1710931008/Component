07c6f003a77a7bc77c5e64a3b69805b5e79d6bfb
kenny <s1710931008@gms.nutc.edu.tw>
2023-07-23 11:22:21 +0800

    account

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const moment = require('moment');
+const AccountModel = require('../models/AccountModel');
+
 //lowdb 導入
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
@@ -13,6 +16,45 @@ router.get('/', function(req, res, next) {
   console.log(str[0].Mouser_Number)
   res.render('index',{str})
 });
+
+
+//记账本的列表
+router.get('/account', function(req, res, next) {
+  //获取所有的账单信息
+  let accounts = db.get('accounts').value();
+  //读取集合信息
+
+  //批量讀取
+  AccountModel.find().sort({time: -1}).then((data) => {
+    // console.log(data); 
+    res.render('./account/list', {accounts: data,moment: moment});
+    // mongoose.disconnect(); 
+  })
+});
+
+//添加记录
+router.get('/account/create', function(req, res, next) {
+  res.render('./account/create');
+});
+
+//新增记录
+router.post('/account', (req, res) => {
+  //插入数据库
+
+  // 新增資料
+  AccountModel.create({ 
+    ...req.body,
+    //修改 time 属性的值
+    time: moment(req.body.time).toDate()
+  })
+  .then(() => { 
+    //成功提醒
+    res.render('./account/success', {msg: '添加成功哦~~~', url: '/account'});
+    console.log('新增成功'); 
+    // mongoose.disconnect(); 
+  });
+});
+
 /* GET home page. */
 router.get('/list/:id', function(req, res, next) {
   const id = req.params.id;
@@ -36,7 +78,7 @@ router.get('/list/:id', function(req, res, next) {
   console.log("aaa:", str.id);
   let ssid = db.get('use').filter({sid:str.id}).value();
   console.log('sid:',ssid)
-  
+
   Object.values(str).forEach(items => {
     // console.log("aaa:", str.id);
     // let ssid = db.get('use').filter({sid:str.id}).value();
